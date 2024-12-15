60f3be98a3935c79b2c3d8f6ea88aab1f69eeaeb
iwi11burn <iwillburn11@icloud.com>
2024-12-15 10:23:18 +0200

    hw4

--- js/main.js
+++ js/main.js
@@ -1,3 +1,17 @@
 $(function () {
+	$('.menu__button, .menu__link').on('click', function () {
+		$('.header').toggleClass('header--active')
+		$('.menu__button').toggleClass('open')
+	})
+
+	$('.menu').on('click', 'a', function (event) {
+		event.preventDefault()
+
+		var id = $(this).attr('href'),
+			top = $(id).offset().top
+
+		$('body,html').animate({ scrollTop: top }, 1500)
+	})
+
 	var mixer = mixitup('.works__images')
 })
