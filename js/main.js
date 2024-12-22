df6bd912ce36b278165e2c80a020190ccce37ef9
iwi11burn <iwillburn11@icloud.com>
2024-12-22 17:17:08 +0200

    add change

--- js/main.js
+++ js/main.js
@@ -1,18 +1,27 @@
 $(function () {
 	$('.menu__button, .menu__link').on('click', function () {
-		$('.header').toggleClass('header--active')
+		$('.menu').toggleClass('menu--active')
 		$('.menu__button').toggleClass('open')
 		$('body').toggleClass('lock')
 	})
 
 	$('.menu').on('click', 'a', function (event) {
 		event.preventDefault()
-
 		var id = $(this).attr('href'),
-			top = $(id).offset().top
+			headerHeight = $('.header').outerHeight(),
+			top = $(id).offset().top - headerHeight
 
 		$('body,html').animate({ scrollTop: top }, 1500)
 	})
 
-	var mixer = mixitup('.works__images')
+	const header = $('.header')
+	$(window).on('scroll', function () {
+		if ($(this).scrollTop() > 50) {
+			header.addClass('header--active')
+		} else {
+			header.removeClass('header--active')
+		}
+	})
 })
+
+var mixer = mixitup('.works__images')
