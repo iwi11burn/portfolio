d8c8201c504e2565dbce400766410333e92ec088
iwi11burn <iwillburn11@icloud.com>
2024-12-23 17:40:54 +0200

    ..

--- js/main.js
+++ js/main.js
@@ -1,34 +1,44 @@
 $(function () {
-	$('.menu__button, .menu__link').on('click', function () {
-		$('.menu').toggleClass('menu--active')
-		$('.menu__button').toggleClass('open')
-		$('body').toggleClass('lock')
+	$(function () {
+		$('.menu__button, .menu__link').on('click', function () {
+			$('.menu').toggleClass('menu--active')
+			$('.menu__button').toggleClass('open')
+
+			if ($('.menu').hasClass('menu--active')) {
+				$('body').addClass('lock')
+			} else {
+				$('body').removeClass('lock')
+			}
+		})
+
+		const header = $('.header')
+		const headerContent = $('.title-page__title')
+		$(window).on('scroll', function () {
+			if ($(this).scrollTop() > 10) {
+				header.addClass('header--active')
+				headerContent.addClass('title-page__title--min')
+			} else {
+				header.removeClass('header--active')
+				headerContent.removeClass('title-page__title--min')
+			}
+		})
+
+		$('.title-page').on(
+			'click',
+			'.menu__link, .header__logo, .title-page__scroll',
+			function (event) {
+				event.preventDefault()
+
+				var id = $(this).attr('href'),
+					headerHeight = $('.header').outerHeight(),
+					top = $(id).offset().top - headerHeight
+
+				$('body').removeClass('lock')
+
+				$('body,html').animate({ scrollTop: top }, 1500)
+			}
+		)
 	})
-
-	const header = $('.header')
-	const headerContent = $('.title-page__title')
-	$(window).on('scroll', function () {
-		if ($(this).scrollTop() > 50) {
-			header.addClass('header--active')
-			headerContent.addClass('title-page__title--min')
-		} else {
-			header.removeClass('header--active')
-			headerContent.removeClass('title-page__title--min')
-		}
-	})
-
-	$('.title-page').on(
-		'click',
-		'.menu__link, .header__logo, .title-page__scroll',
-		function (event) {
-			event.preventDefault()
-			var id = $(this).attr('href'),
-				headerHeight = $('.header').outerHeight(),
-				top = $(id).offset().top - headerHeight
-
-			$('body,html').animate({ scrollTop: top }, 1500)
-		}
-	)
 })
 
 var mixer = mixitup('.works__images')
