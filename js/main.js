e9abc3b0f7f557e5cfe1fa6d3ab7291f151c92c1
iwi11burn <iwillburn11@icloud.com>
2024-12-23 17:24:16 +0200

    add changes

--- js/main.js
+++ js/main.js
@@ -5,23 +5,30 @@ $(function () {
 		$('body').toggleClass('lock')
 	})
 
-	$('.menu').on('click', 'a', function (event) {
-		event.preventDefault()
-		var id = $(this).attr('href'),
-			headerHeight = $('.header').outerHeight(),
-			top = $(id).offset().top - headerHeight
-
-		$('body,html').animate({ scrollTop: top }, 1500)
-	})
-
 	const header = $('.header')
+	const headerContent = $('.title-page__title')
 	$(window).on('scroll', function () {
 		if ($(this).scrollTop() > 50) {
 			header.addClass('header--active')
+			headerContent.addClass('title-page__title--min')
 		} else {
 			header.removeClass('header--active')
+			headerContent.removeClass('title-page__title--min')
 		}
 	})
+
+	$('.title-page').on(
+		'click',
+		'.menu__link, .header__logo, .title-page__scroll',
+		function (event) {
+			event.preventDefault()
+			var id = $(this).attr('href'),
+				headerHeight = $('.header').outerHeight(),
+				top = $(id).offset().top - headerHeight
+
+			$('body,html').animate({ scrollTop: top }, 1500)
+		}
+	)
 })
 
 var mixer = mixitup('.works__images')
